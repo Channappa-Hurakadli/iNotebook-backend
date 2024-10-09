063b3d8b8d9eb968e639e9e825d9e35b3210f25a
Channappa-Hurakadli <channappahurakadli8@gmail.com>
2024-10-09 18:09:05 +0530

    Added endpoint to login with validations(vid50)

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,25 @@
 const express=require('express');
 const User = require('../models/User');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
+
+const jwtSec = 'thisIsASecretString';
 
 //create a user using POST /api/auth/createuser ... no login required
 
-router.post('/createuser',[
+router.post('/createuser',[                               
+
+    //validating the input info field (field,error-msg)
+
     body('name','Enter the valid name').isLength({min:3}),
     body('email','Enter the valid email id').isEmail(),
     body('password','Password must be min 4 characters').isLength({min:4}),
 
 ],async (req,res)=>{
+
+    // if there are errors return the error and 'bad request'
     const result = validationResult(req);
     if (!result.isEmpty()) {
         return res.status(400).json({errors:result.array()});
@@ -23,15 +32,73 @@ router.post('/createuser',[
       return res.status(400).json({error:"Sorry the user with this email already exists"})
     }
 
+    //generating the salt 
+    const salt = await bcrypt.genSalt(10);
+
+    //hashing the password and adding the salt
+    const secPass = await bcrypt.hash(req.body.password,salt);
+
+    //creating the user
   user = await User.create({
     name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
+    email:req.body.email,
+    password: secPass,
   })
-  res.json(user)
+
+  const data = {
+    user: user.id,
+  }
+
+  const authToken = jwt.sign(data,jwtSec);
+  res.json({authToken});
+
+  // res.json(user)
+
   }catch(error){
     console.error(error.message);
-    res.status(500).send("Some error occured");
+    res.status(500).send("Internal Server Error");
   }
+})
+
+//Authenticate a user using POST /api/auth/login ... no login required
+
+router.post('/login',[                               
+
+  //validating the input info field (field,error-msg)
+
+  body('email','Enter the valid email id').isEmail(),
+  body('password','Password must be min 4 characters').isLength({min:4}),
+
+],async (req,res)=>{
+
+    // if there are errors return the error and 'bad request'
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).json({errors:result.array()});
+    }
+
+    const {password,email}=req.body;  //destructuring the user object 
+    try {
+      let user = await User.findOne({email});   // finding the entered email from database
+      if(!user){
+        return res.status(400).json({error:"Please enter the proper credentials"})    //return error if not found
+      } 
+    const passwordCheck = await bcrypt.compare(password,user.password);     // comparing the entered password and userPassword
+    if(!passwordCheck){
+      return res.status(400).json({error:"Please enter the proper credentials"})    //if not match return with error
+    }
+
+    const data = {
+      user: user.id,
+    }
+  
+    const authToken = jwt.sign(data,jwtSec);
+    res.json({authToken});
+      
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+
 })
 module.exports = router;
